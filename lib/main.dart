68d23f1c078b31b844dc4f5c885a0c5aee9e090c
Ananda S <asarangaram@hotmail.com>
2022-11-25 16:18:57 +0530

    add device preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app_router.dart';
 
-void main() => runApp(
-      DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) => const AppRouter(),
-      ),
-    );
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+  return runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const AppRouter(),
+    ),
+  );
+}
