17350edf66cdb3ae68dfe6c77de2431e0d751c63
Ananda S <asarangaram@hotmail.com>
2022-12-06 15:12:00 +0530

    TTSSpeaker class

--- read_out_loud_app/lib/src/pages/words/tts_config.dart
+++ read_out_loud_app/lib/src/pages/words/tts_config.dart
@@ -1,15 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 // ignore_for_file: avoid_print
 
-import 'dart:async';
 import 'dart:io' show Platform;
+
 import 'package:flutter/foundation.dart' show kIsWeb;
-
 import 'package:flutter/material.dart';
-import 'package:flutter_tts/flutter_tts.dart';
 
-class TTSPlayAudio {
-  late FlutterTts flutterTts;
-}
+import 'tts_speaker.dart';
 
 class TTSConfig extends StatefulWidget {
   final Size size;
@@ -19,24 +16,8 @@ class TTSConfig extends StatefulWidget {
   TTSConfigState createState() => TTSConfigState();
 }
 
-enum TtsState { playing, stopped, paused, continued }
-
 class TTSConfigState extends State<TTSConfig> {
-  late FlutterTts flutterTts;
-  String? language;
-  String? engine;
-  double volume = 0.5;
-  double pitch = 1.0;
-  double rate = 0.5;
-  bool isCurrentLanguageInstalled = false;
-
-  TtsState ttsState = TtsState.stopped;
-
-  get isPlaying => ttsState == TtsState.playing;
-  get isStopped => ttsState == TtsState.stopped;
-  get isPaused => ttsState == TtsState.paused;
-  get isContinued => ttsState == TtsState.continued;
-
+  late TTSSpeaker speaker;
   bool get isIOS => !kIsWeb && Platform.isIOS;
   bool get isAndroid => !kIsWeb && Platform.isAndroid;
   bool get isWindows => !kIsWeb && Platform.isWindows;
@@ -45,132 +26,29 @@ class TTSConfigState extends State<TTSConfig> {
   @override
   initState() {
     super.initState();
-    initTts();
-  }
-
-  initTts() {
-    flutterTts = FlutterTts();
-
-    _setAwaitOptions();
-    flutterTts.setLanguage('en-US');
-
-    if (isAndroid) {
-      _getDefaultEngine();
-      _getDefaultVoice();
-    }
-
-    flutterTts.setStartHandler(() {
-      setState(() {
-        print("Playing");
-        ttsState = TtsState.playing;
-      });
-    });
-
-    if (isAndroid) {
-      flutterTts.setInitHandler(() {
-        setState(() {
-          print("TTS Initialized");
-        });
-      });
-    }
-
-    flutterTts.setCompletionHandler(() {
-      setState(() {
-        print("Complete");
-        ttsState = TtsState.stopped;
-      });
-    });
-
-    flutterTts.setCancelHandler(() {
-      setState(() {
-        print("Cancel");
-        ttsState = TtsState.stopped;
-      });
-    });
-
-    flutterTts.setPauseHandler(() {
-      setState(() {
-        print("Paused");
-        ttsState = TtsState.paused;
-      });
-    });
-
-    flutterTts.setContinueHandler(() {
-      setState(() {
-        print("Continued");
-        ttsState = TtsState.continued;
-      });
-    });
-
-    flutterTts.setErrorHandler((msg) {
-      setState(() {
-        print("error: $msg");
-        ttsState = TtsState.stopped;
-      });
-    });
-  }
-
-  Future _getDefaultEngine() async {
-    var engine = await flutterTts.getDefaultEngine;
-    if (engine != null) {
-      print(engine);
-    }
-  }
-
-  Future _getDefaultVoice() async {
-    var voice = await flutterTts.getDefaultVoice;
-    if (voice != null) {
-      print(voice);
-    }
-  }
-
-  Future _speak(String text) async {
-    await flutterTts.setVolume(volume);
-    await flutterTts.setSpeechRate(rate);
-    await flutterTts.setPitch(pitch);
-
-    if (text.isNotEmpty) {
-      await flutterTts.speak(text);
-    }
-  }
-
-  Future _setAwaitOptions() async {
-    await flutterTts.awaitSpeakCompletion(true);
-  }
-
-  Future _stop() async {
-    var result = await flutterTts.stop();
-    if (result == 1) setState(() => ttsState = TtsState.stopped);
-  }
-
-  Future _pause() async {
-    var result = await flutterTts.pause();
-    if (result == 1) setState(() => ttsState = TtsState.paused);
+    speaker = TTSSpeaker();
+    speaker.init(isAndroid: isAndroid);
   }
 
   @override
   void dispose() {
     super.dispose();
-    flutterTts.stop();
+    speaker.flutterTts.stop();
   }
 
   @override
   Widget build(BuildContext context) {
-    return _btnSection();
-  }
-
-  Widget _btnSection() {
     return SizedBox.fromSize(
       size: widget.size,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           _buildButtonColumn(Colors.green, Colors.greenAccent, Icons.play_arrow,
-              'PLAY', () => _speak("Can you read this word?")),
+              'PLAY', () => speaker.speak("Can you read this word?")),
           _buildButtonColumn(
-              Colors.red, Colors.redAccent, Icons.stop, 'STOP', _stop),
-          _buildButtonColumn(
-              Colors.blue, Colors.blueAccent, Icons.pause, 'PAUSE', _pause),
+              Colors.red, Colors.redAccent, Icons.stop, 'STOP', speaker.stop),
+          _buildButtonColumn(Colors.blue, Colors.blueAccent, Icons.pause,
+              'PAUSE', speaker.pause),
         ],
       ),
     );
