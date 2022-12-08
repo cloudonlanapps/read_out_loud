491e900d4f399524db41a0705a86cd105f3542db
Ananda S <asarangaram@hotmail.com>
2022-12-08 18:53:39 +0530

    Handle Size(0.0, 0.0)

--- route_manager/lib/src/screen_background.dart
+++ route_manager/lib/src/screen_background.dart
@@ -17,6 +17,27 @@ class ScreenBackground extends StatelessWidget {
 
     final padding = MediaQuery.of(context).size.shortestSide * 0.01;
     final double borderRadius = viewConfig.borderRadius ?? 15;
+
+    /// Flutter starts quicker in release mode and the native platform
+    /// reports the actual resolution with a delay. So the very first frame
+    /// is build with Size(0.0, 0.0). After sometime, the app will be
+    /// rebuilt again with the proper value. Print MediaQuery.context.size
+    /// in the build() to observe this. (you need to use the regular print()
+    /// in your app to debug. Make the code to be resilient against
+    /// Size(0.0, 0.0) and make sure that you call findRenderBox only after
+    /// the resolution is received from the platform will make sure that you
+    /// get correct size.
+    /// Refer this https://github.com/flutter/flutter/issues/25827
+    ///
+    /// Alternate way using RenderProxyBox can also be explored.
+    /// https://stackoverflow.com/questions/49307677/how-to-get-height-of-a-widget
+
+    if (MediaQuery.of(context).size.width < 10 ||
+        MediaQuery.of(context).size.height < 10) {
+      return Scaffold(
+        body: Container(),
+      );
+    }
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: Stack(
