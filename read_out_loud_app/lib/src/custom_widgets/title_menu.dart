6e4b064e0beb0da709779a2cf4f29a05ee2bf6b1
Ananda S <asarangaram@hotmail.com>
2022-12-14 09:13:04 +0530

    Title Menu Custom Widget

---
+++ read_out_loud_app/lib/src/custom_widgets/title_menu.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class TitleMenu extends StatelessWidget {
+  final String title;
+  final Size size;
+  final Function() onClose;
+
+  const TitleMenu(
+      {super.key,
+      required this.title,
+      required this.onClose,
+      required this.size});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.fromSize(
+      size: size,
+      child: Row(
+        children: [
+          IconButton(
+              onPressed: onClose,
+              icon: Icon(
+                Icons.arrow_back,
+                size: Theme.of(context).textTheme.displayMedium!.fontSize,
+              )),
+          Expanded(
+            child: Center(
+              child: /*CHECK*/ Text(
+                title,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
